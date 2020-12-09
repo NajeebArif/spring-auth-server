f11a9cec6aa8ce237244387c581c4198d574ce0d
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-09 20:44:46 +0530

    Update AuthorizationServerConfig.java
    Wiring the AuthenticationManager

--- src/main/java/narif/manslp/msoauth2/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/narif/manslp/msoauth2/authserver/config/AuthorizationServerConfig.java
@@ -1,8 +1,19 @@
 package narif.manslp.msoauth2.authserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 }
