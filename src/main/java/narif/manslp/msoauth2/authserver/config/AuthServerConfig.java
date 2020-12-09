d1669529878033b051d717f0ea3ffce95914fdef
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-09 22:53:48 +0530

    Update AuthServerConfig.java
    To generate Signed JWTs, using private key of a asymmetric key pair

--- src/main/java/narif/manslp/msoauth2/authserver/config/AuthServerConfig.java
+++ src/main/java/narif/manslp/msoauth2/authserver/config/AuthServerConfig.java
@@ -1,7 +1,10 @@
 package narif.manslp.msoauth2.authserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -9,6 +12,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.client.InMemoryClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import java.util.Map;
 import java.util.Set;
@@ -17,20 +24,44 @@ import java.util.Set;
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Value("${keypair.alias}")
+    private String alias;
+    @Value("${keypair.keystore}")
+    private String keyStore;
+    @Value("${keypair.storepass}")
+    private String storepass;
+
+
     @Autowired
     private AuthenticationManager authenticationManagerBean;
 
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManagerBean);
-    }
-
-    @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        var inMemoryClientDetailsService = getInMemoryClientDetailsService();
+        final var inMemoryClientDetailsService = getInMemoryClientDetailsService();
         clients.withClientDetails(inMemoryClientDetailsService);
     }
 
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManagerBean)
+                .tokenStore(tokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+        final var jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        final var keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource(keyStore), storepass.toCharArray());
+        jwtAccessTokenConverter.setKeyPair(keyStoreKeyFactory.getKeyPair(alias));
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    //<editor-fold desc="HELPER METHODS">
     private InMemoryClientDetailsService getInMemoryClientDetailsService() {
         var inMemoryClientDetailsService = new InMemoryClientDetailsService();
         var baseClientDetails = getBaseClientDetails();
@@ -46,4 +77,5 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
         clientDetails.setAuthorizedGrantTypes(Set.of("password"));
         return clientDetails;
     }
+//</editor-fold>
 }
