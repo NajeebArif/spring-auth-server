0b3f0844258a86d6bab0a6b9d0ccf5afdcaa5f9b
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-09 20:53:22 +0530

    Update AuthorizationServerConfig.java
    Adding ClientDetailsService - UserDetailsService equivalent of OAuth2 :smiley:

--- src/main/java/narif/manslp/msoauth2/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/narif/manslp/msoauth2/authserver/config/AuthorizationServerConfig.java
@@ -2,9 +2,15 @@ package narif.manslp.msoauth2.authserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.InMemoryClientDetailsService;
+
+import java.util.List;
+import java.util.Map;
 
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -15,5 +21,23 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        var inMemoryClientDetailsService = getInMemoryClientDetailsService();
+
+        clients.withClientDetails(inMemoryClientDetailsService);
+    }
+
+    private InMemoryClientDetailsService getInMemoryClientDetailsService() {
+        var inMemoryClientDetailsService = new InMemoryClientDetailsService();
+        var baseClientDetails = new BaseClientDetails();
+        baseClientDetails.setClientId("clientId");
+        baseClientDetails.setClientSecret("clientSecret");
+        baseClientDetails.setScope(List.of("read"));
+        baseClientDetails.setAuthorizedGrantTypes(List.of("password"));
+        inMemoryClientDetailsService.setClientDetailsStore(Map.of("client",baseClientDetails));
+        return inMemoryClientDetailsService;
     }
 }
