90c89cbf2228f0bf1c95835faabdf08617aa2a3b
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-09 19:37:14 +0530

    Adding the project files

---
+++ src/test/java/narif/manslp/msoauth2/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package narif.manslp.msoauth2.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
