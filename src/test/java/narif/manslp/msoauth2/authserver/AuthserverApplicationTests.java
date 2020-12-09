7053e69585be45cbb9625aa41bb5229082d2f693
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-09 22:36:07 +0530

    Update AuthserverApplicationTests.java
    Adding test case to verify if the authorization server can issue access token for password grant flow.

--- src/test/java/narif/manslp/msoauth2/authserver/AuthserverApplicationTests.java
+++ src/test/java/narif/manslp/msoauth2/authserver/AuthserverApplicationTests.java
@@ -1,13 +1,42 @@
 package narif.manslp.msoauth2.authserver;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Auth Server Specs:")
 class AuthserverApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("If proper client details and user details are provided, then access token should be returned.")
+	public void testValidTokenGen() throws Exception {
+		mockMvc.perform(post("/oauth/token")
+				.queryParam("grant_type","password")
+				.queryParam("username","narif")
+				.queryParam("password","strongPassword")
+				.queryParam("scope","read")
+				.with(SecurityMockMvcRequestPostProcessors.httpBasic("clientId","clientSecret"))
+		).andExpect(status().isOk());
+	}
+
+	@Test
+	@DisplayName("Call to token url should return 401, if client info is missing.")
+	void testAuthTokenUrlAccess() throws Exception{
+		mockMvc.perform(get("/oauth/token"))
+				.andExpect(status().isUnauthorized());
 	}
 
 }
