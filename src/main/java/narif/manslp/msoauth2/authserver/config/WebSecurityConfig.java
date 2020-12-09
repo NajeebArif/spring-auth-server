928dc51769dc3868c84d295f8cdfa74eb38d82c6
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-10 03:29:59 +0530

    changed the noop password encoder to bCryptPasswordEncoder

--- src/main/java/narif/manslp/msoauth2/authserver/config/WebSecurityConfig.java
+++ src/main/java/narif/manslp/msoauth2/authserver/config/WebSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
