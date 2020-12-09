71206f70418f13f2ac5828f5de19af1df19e8793
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-09 20:42:20 +0530

    Create AuthorizationServerConfig.java

---
+++ src/main/java/narif/manslp/msoauth2/authserver/config/AuthorizationServerConfig.java
@@ -0,0 +1,8 @@
+package narif.manslp.msoauth2.authserver.config;
+
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+}
