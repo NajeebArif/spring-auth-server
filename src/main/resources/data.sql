4ff09eaa37c05a30455d3b394b55fedd1bd6ace9
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-11 02:58:30 +0530

    fixing the seed data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,14 +7,14 @@ INSERT IGNORE INTO auth_server_db.authority (id, name, user) VALUES (2, "WRITE",
 insert ignore into oauth2_clients (id,client_id, client_secret) VALUES (1, 'clientId','clientSecret');
 insert ignore into oauth2_clients (id,client_id, client_secret) VALUES (2,'machineClientId','machineClientSecret');
 
-insert ignore into scope (name, client) VALUES ('read',1);
-insert  ignore into scope (name, client) VALUES ('read',2);
-insert ignore into scope (name, client) VALUES ('write',1);
-insert ignore into scope (name, client) VALUES ('delete',1);
+insert ignore into scope (id,name, client) VALUES (1,'read',1);
+insert ignore into scope (id,name, client) VALUES (2,'write',1);
+insert ignore into scope (id,name, client) VALUES (3,'delete',1);
+insert ignore into scope (id,name, client) VALUES (4,'read',2);
 
-insert ignore into grant_types(name, client) VALUES ('password',1);
-insert ignore into grant_types(name, client) VALUES ('client_credentials',2);
-insert ignore into grant_types(name, client) VALUES ('refresh_token',1);
-insert ignore into grant_types(name, client) VALUES ('authorization_code',1);
+insert ignore into grant_types(id,name, client) VALUES (1,'password',1);
+insert ignore into grant_types(id,name, client) VALUES (2,'refresh_token',1);
+insert ignore into grant_types(id,name, client) VALUES (3,'authorization_code',1);
+insert ignore into grant_types(id,name, client) VALUES (4,'client_credentials',2);
 
-insert into redirect_url (url, client) VALUES ('http://localhost:8080/greet',1);
+insert into redirect_url (id,url, client) VALUES (1,'http://localhost:8080/greet',1);
