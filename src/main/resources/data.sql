3ff288cdbb359b73112798ba7fa35e65347f946e
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-11 02:39:55 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,19 @@ INSERT IGNORE INTO auth_server_db.user (id, username, password, algorithm) VALUE
 
 INSERT IGNORE INTO auth_server_db.authority (id, name, user) VALUES (1, "READ", 1);
 INSERT IGNORE INTO auth_server_db.authority (id, name, user) VALUES (2, "WRITE", 1);
+
+
+insert into oauth2_clients (client_id, client_secret) VALUES ('clientId','clientSecret');
+insert into oauth2_clients (client_id, client_secret) VALUES ('machineClientId','machineClientSecret');
+
+insert into scope (name, client) VALUES ('read',1);
+insert into scope (name, client) VALUES ('read',2);
+insert into scope (name, client) VALUES ('write',1);
+insert into scope (name, client) VALUES ('delete',1);
+
+insert into grant_types(name, client) VALUES ('password',1);
+insert into grant_types(name, client) VALUES ('client_credentials',2);
+insert into grant_types(name, client) VALUES ('refresh_token',1);
+insert into grant_types(name, client) VALUES ('authorization_code',1);
+
+insert into redirect_url (url, client) VALUES ('http://localhost:8080/greet',1);
