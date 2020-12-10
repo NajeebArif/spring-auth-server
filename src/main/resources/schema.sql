e2ba1011b97902289f6891eee5f7f5b858932ba0
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-11 02:35:41 +0530

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,3 +10,29 @@ CREATE TABLE if NOT EXISTS auth_server_db.authority (
   name VARCHAR(45) NOT NULL,
   user INT NOT NULL,
   PRIMARY KEY (id));
+
+CReate TABLE if not exists auth_server_db.oauth2_clients(
+    id int not null AUTO_INCREMENT,
+    client_id varchar(100) not null ,
+    client_secret varchar(100) not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE if NOT EXISTS auth_server_db.scope (
+                                                        id INT NOT NULL AUTO_INCREMENT,
+                                                        name VARCHAR(45) NOT NULL,
+                                                        client INT NOT NULL,
+                                                        PRIMARY KEY (id));
+
+CREATE TABLE if NOT EXISTS auth_server_db.grant_types (
+                                                    id INT NOT NULL AUTO_INCREMENT,
+                                                    name VARCHAR(45) NOT NULL,
+                                                    client INT NOT NULL,
+                                                    PRIMARY KEY (id));
+
+CREATE TABLE if NOT EXISTS auth_server_db.redirect_url (
+                                                    id INT NOT NULL AUTO_INCREMENT,
+                                                    url VARCHAR(1000) NOT NULL,
+                                                    client INT NOT NULL,
+                                                    PRIMARY KEY (id));
+
