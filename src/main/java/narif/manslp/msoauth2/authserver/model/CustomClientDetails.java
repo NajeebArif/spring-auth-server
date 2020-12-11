8aaea4362120ed877e0ac5e0c25df916d349ca36
Najeeb Arif <NajeebArif786@outlook.com>
2020-12-11 06:54:25 +0530

    Create CustomClientDetails.java

---
+++ src/main/java/narif/manslp/msoauth2/authserver/model/CustomClientDetails.java
@@ -0,0 +1,89 @@
+package narif.manslp.msoauth2.authserver.model;
+
+import narif.manslp.msoauth2.authserver.entities.client.Client;
+import narif.manslp.msoauth2.authserver.entities.client.GrantType;
+import narif.manslp.msoauth2.authserver.entities.client.RedirectUrl;
+import narif.manslp.msoauth2.authserver.entities.client.Scope;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.provider.ClientDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CustomClientDetails implements ClientDetails {
+
+    private Client client;
+
+    public CustomClientDetails(Client client) {
+        this.client = client;
+    }
+
+    @Override
+    public String getClientId() {
+        return client.getClientId();
+    }
+
+    @Override
+    public Set<String> getResourceIds() {
+        return Collections.EMPTY_SET;
+    }
+
+    @Override
+    public boolean isSecretRequired() {
+        return false;
+    }
+
+    @Override
+    public String getClientSecret() {
+        return client.getClientSecret();
+    }
+
+    @Override
+    public boolean isScoped() {
+        return true;
+    }
+
+    @Override
+    public Set<String> getScope() {
+        return client.getScopes().stream().map(Scope::getName).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<String> getAuthorizedGrantTypes() {
+        return client.getGrantTypes().stream().map(GrantType::getName).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<String> getRegisteredRedirectUri() {
+        return client.getRedirectUrls().stream().map(RedirectUrl::getUrl).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<GrantedAuthority> getAuthorities() {
+        return client.getScopes().stream().map(Scope::getName).map(SimpleGrantedAuthority::new).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Integer getAccessTokenValiditySeconds() {
+        return 3600;
+    }
+
+    @Override
+    public Integer getRefreshTokenValiditySeconds() {
+        return 3600;
+    }
+
+    @Override
+    public boolean isAutoApprove(String s) {
+        return false;
+    }
+
+    @Override
+    public Map<String, Object> getAdditionalInformation() {
+        return null;
+    }
+}
